09a5151b959528b8701540b71214d002ae971a00
andreapiras00 <andrea.piras00@gmail.com>
2023-03-26 14:24:09 +0000

    trial

--- scale-up/run_scale_up.py
+++ scale-up/run_scale_up.py
@@ -14,8 +14,8 @@ TIMES = 5
 #gpus = [1,2,4,8]
 gpus = [1] # avoid running with higher number of GPUs until required
 scale = ["strong","weak"]
-schms = ["scale-up"]
 #schms = ["scale-up", "scale-up-nvlink"] # commented since only scale-up will be used
+schms = ["scale-up"]
 #====================================================================
 
 apps = []
