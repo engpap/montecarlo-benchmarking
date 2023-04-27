b02e379e40859698c45595430b2a3df077ed67e4
engpap <10701904@polimi.it>
2023-04-27 18:18:40 +0000

    minor changes

--- scale-up/scale-up/montecarlo/run.sh
+++ scale-up/scale-up/montecarlo/run.sh
@@ -1,1 +1,14 @@
-./MonteCarlo --scaling=strong --method=threaded
+make clean
+make
+./MonteCarlo --scaling=weak --method=streamed #cold start
+./MonteCarlo --scaling=weak --method=streamed
+./MonteCarlo --scaling=weak --method=streamed
+./MonteCarlo --scaling=weak --method=streamed
+./MonteCarlo --scaling=weak --method=streamed
+./MonteCarlo --scaling=weak --method=streamed
+./MonteCarlo --scaling=weak --method=streamed
+./MonteCarlo --scaling=weak --method=streamed
+./MonteCarlo --scaling=weak --method=streamed
+./MonteCarlo --scaling=weak --method=streamed
+./MonteCarlo --scaling=weak --method=streamed
+nvprof ./MonteCarlo --scaling=weak --method=streamed # for page fault reporting
