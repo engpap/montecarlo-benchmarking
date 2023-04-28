98f96dcbb121de86fc8722faa4461afc2b6092fb
andreapiras00 <andrea.piras00@gmail.com>
2023-04-28 15:31:27 +0000

    Added nvprof outputs for x32 option size

--- scale-up/scale-up/montecarlo_UMv1/run.sh
+++ scale-up/scale-up/montecarlo_UMv1/run.sh
@@ -3,9 +3,9 @@ export CUDA_VISIBLE_DEVICES=0,1
 make clean
 make
 
-for scaling in weak strong
+for scaling in strong
 do
-    for method in threaded streamed
+    for method in streamed
     do
         ./MonteCarlo --scaling=$scaling --method=$method
         ./MonteCarlo --scaling=$scaling --method=$method
@@ -18,7 +18,7 @@ do
         ./MonteCarlo --scaling=$scaling --method=$method
         ./MonteCarlo --scaling=$scaling --method=$method
         ./MonteCarlo --scaling=$scaling --method=$method
-        nvprof --log-file ./nvprof/nvprof_2g_x32_${scaling}_${method}.txt ./MonteCarlo --scaling=$scaling --method=$method
+        nvprof --log-file ./nvprof/nvprof_2g_x32_${scaling}_${method}_UMv1.txt ./MonteCarlo --scaling=$scaling --method=$method
         #nsys profile -o report_2g_x32_${scaling}_${method}_UMv1 --stats=true --cuda-memory-usage=true --cuda-um-cpu-page-faults=true --cuda-um-gpu-page-faults=true --force-overwrite=true ./MonteCarlo --scaling=$scaling --method=$method 
     done
 done
--- scale-up/scale-up/montecarlo_UMv2/run.sh
+++ scale-up/scale-up/montecarlo_UMv2/run.sh
@@ -8,7 +8,17 @@ do
     for method in threaded streamed
     do
         ./MonteCarlo --scaling=$scaling --method=$method
-        nvprof --log-file nvprof_2g_{$scaling}_{$method}_compact.txt ./MonteCarlo --scaling=$scaling --method=$method
+        ./MonteCarlo --scaling=$scaling --method=$method
+        ./MonteCarlo --scaling=$scaling --method=$method
+        ./MonteCarlo --scaling=$scaling --method=$method
+        ./MonteCarlo --scaling=$scaling --method=$method
+        ./MonteCarlo --scaling=$scaling --method=$method
+        ./MonteCarlo --scaling=$scaling --method=$method
+        ./MonteCarlo --scaling=$scaling --method=$method
+        ./MonteCarlo --scaling=$scaling --method=$method
+        ./MonteCarlo --scaling=$scaling --method=$method
+        ./MonteCarlo --scaling=$scaling --method=$method
+        nvprof --log-file ./nvprof/nvprof_2g_x32_${scaling}_${method}_UMv2.txt ./MonteCarlo --scaling=$scaling --method=$method
         #nsys profile -o report_2g_{$scaling}_{$method}_UMv2 --stats=true --cuda-memory-usage=true --cuda-um-cpu-page-faults=true --cuda-um-gpu-page-faults=true --force-overwrite=true ./MonteCarlo --scaling=$scaling --method=$method 
     done
 done
